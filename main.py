7ec0f232db7c03ee2bed8714611c70319c82df43
skprot <sayanprotasov@gmail.com>
2020-09-03 00:52:39 +0300

    Updates with time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+import datetime
 from photosAPI.photos_grabber import GooglePhotoService
 
 token = '<TOKEN>'
@@ -20,11 +21,21 @@ def photo(msg):
 
 
 def test_send_message():
-    text = 'CI Test Message'
+    media.download_random_photo()
+    img = open('downloads/photo.jpg', 'rb')
+
+    text = 'Вот пришла кратная минута двум))'
     ret_msg = bot.send_message(25035106, text)
+    bot.send_photo(25035106, img, caption='Держи фотку')
     assert ret_msg.message_id
 
+
 if __name__ == '__main__':
+    past_minute = -1
+
     while True:
-        #if TIME
-        test_send_message()
+        now = datetime.datetime.now()
+
+        if now.minute % 2 == 0 and now.minute != past_minute:
+            past_minute = now.minute
+            test_send_message()
