c0b9e074d78e562e6eec9da466483003fd2be571
skprot <sayanprotasov@gmail.com>
2021-01-13 15:08:18 +0800

    deploy on heroku 2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 main.py
+web: echo "a"
--- photosAPI/client_creator.py
+++ photosAPI/client_creator.py
@@ -29,7 +29,7 @@ def create_service(client_secret_file, api_name, api_version, *scopes):
             flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
             #cred = flow.run_local_server()
             print('waiting ps:exec')
-            time.sleep(60)
+            time.sleep(20)
             cred = flow.run_console()
 
         with open(pickle_file, 'wb') as token:
